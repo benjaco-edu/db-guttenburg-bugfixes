c6d79eb30715f46995438365552d082560a7760b
bslcphbussiness <cph-bl135@cphbusiness.dk>
2019-05-29 02:46:02 +0200

    mvp done

--- app/index.js
+++ app/index.js
@@ -74,12 +74,12 @@ let queries = {
         ],
         [
             "Locations",
-            coords => { // coords is a 2 element array
+            (coords, range) => { // coords is a 2 element array
                 return [
                     {$geoNear: {
                          near: { type: "Point", coordinates: coords},
                          distanceField: "distance",
-                         maxDistance: 200*1000,
+                         maxDistance: range*1000,
                          spherical: true,
                          key: "coordinate"
                       }},
@@ -98,43 +98,100 @@ let queries = {
             }
         ]
     ],
-    mysql: [
-        `with cities as (select * from Locations where name = ?)
-        select distinct BookParts.id, BookParts.title, BookParts.author from cities 
-        left join BookLocations on BookLocations.location_id = cities.id
-        left join BookParts on BookParts.id = BookLocations.bookparts_id`,
+mysql: [
+`with cities as (
+    select * 
+    from Locations 
+    where name = ?
+)
+select distinct BookParts.id, BookParts.title,BookParts.author 
+from cities 
+left join BookLocations on BookLocations.location_id = cities.id
+left join BookParts     on BookParts.id = BookLocations.bookparts_id`,
 
-        `with selectedtitles as (select * from BookParts where title = ? limit 1)
-        select selectedtitles.id, title, part, author, location_id, index_in_book, name, ST_AsText(coordinate), population, timezone from selectedtitles
-        left join BookLocations on BookLocations.bookparts_id = selectedtitles.id
-        left join Locations on Locations.id = BookLocations.location_id`,
+`with selectedtitles as (
+    select * 
+    from BookParts
+    where title = ? 
+    limit 1
+)
+select selectedtitles.id,  title,  part,   author,     location_id, 
+       index_in_book,      name,   ST_AsText(coordinate) as coordinate, 
+       population,         timezone 
+from selectedtitles
+left join BookLocations on BookLocations.bookparts_id = selectedtitles.id
+left join Locations     on Locations.id = BookLocations.location_id`,
 
-        `with author as (select * from BookParts where author = ?),
-        locs as (select id, JSON_OBJECT("name", name, "population", population, "location", ST_AsText(coordinate)) as locObj from Locations)
-        select author.id, title, part, author, json_arrayagg(locObj) as "locations" from author
-        left join BookLocations on BookLocations.bookparts_id = author.id
-        left join locs on BookLocations.location_id = locs.id
-        group by author.id`,
+`with author as (
+    select * 
+    from BookParts 
+    where author = ?
+),
+locs as (
+    select id, 
+    JSON_OBJECT("name", name, 
+                "population", population,   
+                "location", ST_AsText(coordinate)) as locObj 
+    from Locations
+)
+select author.id,   title,  part,   author, json_arrayagg(locObj) as "locations" 
+from author
+left join BookLocations on BookLocations.bookparts_id = author.id
+left join locs          on BookLocations.location_id = locs.id
+group by author.id`,
 
-        `with cities as (select *, ST_Distance(ST_GeomFromText(?, 4326), coordinate)/1000 as km_away from Locations where ST_Contains(ST_GeomFromText(ST_AsText(ST_Buffer(ST_GeomFromText(?, 0), 125/111.226)), 4326), coordinate))
-        select distinct BookParts.id, title, part, author,  ST_AsText(coordinate) as point from cities
-        inner join BookLocations  on cities.id = BookLocations.location_id
-        left join BookParts on BookParts.id = BookLocations.bookparts_id`
+`with cities as (
+    select *, ST_Distance(ST_GeomFromText(?, 4326), coordinate)/1000 as km_away 
+    from Locations where 
+    ST_Contains(ST_GeomFromText(ST_AsText(ST_Buffer(ST_GeomFromText(?, 0), ?/111.226)), 4326), coordinate)
+)
+select distinct BookParts.id, title, part, author,  ST_AsText(coordinate) as point, km_away, name
+from cities
+inner join BookLocations  on cities.id = BookLocations.location_id
+left join BookParts       on BookParts.id = BookLocations.bookparts_id
+order by km_away`
     ]
 }
 
+app.get("/ex1/loc/:engine/:param", (req, res)=>{
+    let engine = req.params.engine,
+        param = req.params.param;
+    
+    if(engine === "mysql"){
+        connection.query(
+            "SELECT id,  population,  ST_AsText(coordinate) as locObj from Locations where name = ?",
+            [param],
+            function(err, results) {
+                if(err){
+                    return res.json({error: "Error executing query: ", err})
+                }
+                res.json(results)
+            }
+            );
+            return;
+    }else {
+        db.collection("Locations").aggregate([
+            {$match:{name: param}},
+            {$limit:1},
+            {$project: {coordinate: 1, _id:0}}
+            ]).toArray(function(err, data) {
+                res.json(data)   
+            })
+            return
+            
+    }
+
+})
+
 app.get('/execute/3/:engine/:lat/:lng/:range', (req, res)=>{
     let engine = req.params.engine,
-        lat = req.params.lat,
-        lng = req.params.lng,
-        range = req.params.range;
+        lat = parseFloat(req.params.lat),
+        lng = parseFloat(req.params.lng),
+        range = parseFloat(req.params.range);
 
-console.log("query", query);
-console.log("engine", engine);
-console.log("lat", lat);
-console.log("lng", lng);
-console.log("range", range);
-        
+    let timeStart = Date.now();
+
+    console.log("location search ")
 
     let queryToExecture = queries[engine][3];
 
@@ -142,7 +199,7 @@ console.log("range", range);
         let point = `POINT(${lat} ${lng})`
         connection.query(
             queryToExecture,
-            [point, point],
+            [point, point, range],
             function(err, results) {
                 let time = Date.now() - timeStart;
                 res.json({
@@ -156,7 +213,7 @@ console.log("range", range);
     }else{
         let query = queryToExecture[1]([
             lng, lat
-        ]);
+        ], range);
         db.collection(queryToExecture[0]).aggregate(query).toArray(function(err, data) {
             let time = Date.now() - timeStart;
             res.json({
--- app/static/gmaps.js
+++ app/static/gmaps.js
@@ -6,36 +6,58 @@ function myMap(lat, lng){
         zoom:3,
       };
     map = new google.maps.Map(document.getElementById("map"), mapProp);
-
-    map.addListener('click', function(event) {
-      let radius = document.getElementById('rangeSlider').value;
-      placeMarker(event.latLng, map, radius );
-    });
-  
  
 }
 
-function placeMarker(position, map, radius) {
-  var marker = new google.maps.Marker({
-      position: position,
+let objects = []
+function addPin(loc, addToCollection = true){
+  if(typeof loc === "string"){
+    let parse = loc.match(/POINT\((-?\d*\.\d*) (-?\d*\.\d*)\)/);
+    if(parse[1] === undefined){
+      console.error(loc, " is not a mysql point")
+    }
+    loc = {lat: parseFloat(parse[1]),lng:parseFloat(parse[2])}
+  }else{
+    loc = {lat: loc[1],lng:loc[0]}
+  }
+  let marker = new google.maps.Marker({
+      position: loc,
       map: map
   });
+  if(addToCollection)
+    objects.push(marker)
 
-  var cityCircle = new google.maps.Circle({
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
+  return marker;
+}
+
+function addCircle(loc, radius = 10){
+  if(typeof loc === "string"){
+    let parse = loc.match(/POINT\((-?\d*\.\d*) (-?\d*\.\d*)\)/);
+    loc = {lat: parseFloat(parse[1]),lng:parseFloat(parse[2])}
+  }else{
+    loc = {lat: loc[1],lng:loc[0]}
+  }
+  
+  var marker = new google.maps.Circle({
+    strokeColor: '#000',
+    strokeOpacity: 0.5,
     strokeWeight: 2,
-    fillColor: '#FF0000',
+    fillColor: '#000',
     fillOpacity: 0.35,
     map: map,
-    center: position,
+    center: loc,
     radius: radius*1000
   });
 
 
+  objects.push(marker)
 
+  return marker;
+}
 
-  map.panTo(position);
-
-  
+function clearMap(){
+  for(let i of objects){
+    i.setMap(null)
+  }
+  objects = []
 }
